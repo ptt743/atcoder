9908de272ac844904fecdb766dd05982ed1ab73e
ptt743 <55840736+ptt743@users.noreply.github.com>
2023-04-15 21:30:34 +0700

    init

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<queue>
+#include<stack>
+using namespace std;
+//*****taipt*****//
+
+void solve(){
+	
+}
+ 
+int main() {
+    solve();
+    return 0;
+}
