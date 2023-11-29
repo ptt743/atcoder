abb95e7458e5f8961e966999782f15f69214ee1e
PHAM TAN TAI <phamtantai1007@PHAMs-MacBook-Pro.local>
2023-11-30 01:00:46 +0700

    update

--- problems/abc330_c.cpp
+++ problems/abc330_c.cpp
@@ -4,6 +4,7 @@
 #include<string>
 #include<queue>
 #include<stack>
+#include<cmath>
 #include <atcoder/all>
 using namespace std;
 using namespace atcoder;
@@ -11,7 +12,19 @@ using namespace atcoder;
 using mint = modint998244353;
 
 void solve(){
-	
+	long long n;
+	cin>> n;
+	long long ans = n;
+	for(int i = 0; i<=sqrt(n);i++){
+		long long y = sqrt(n - (long long)i*i);
+		long long temp = y;
+		ans = min(ans, abs(i*i + temp*temp - n));
+		temp = y +1;
+		ans = min(ans,abs(i*i + temp*temp - n));
+		temp = y-1;
+		ans = min(ans, abs(i*i + temp*temp -n));
+	}
+	cout<< ans<< endl;
 }
  
 int main() {
