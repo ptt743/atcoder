63792bfd2ca61512d24eb4c100b1718974fdc804
taipt3 <taipt3@vng.com.vn>
2024-07-08 13:42:11 +0700

    update

---
+++ problems/abc361_e.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<queue>
+#include<stack>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+//*****taipt*****//
+using mint = modint998244353;
+
+void solve(){
+		
+}
+ 
+int main() {
+    solve();
+    return 0;
+}
