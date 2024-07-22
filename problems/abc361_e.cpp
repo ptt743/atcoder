408cce8b862b377df321ff45b971066d3eea407b
PHAM TAN TAI <phamtantai1007@192.168.1.5>
2024-07-22 08:10:10 +0700

    update

--- problems/abc361_e.cpp
+++ problems/abc361_e.cpp
@@ -10,10 +10,34 @@ using namespace atcoder;
 //*****taipt*****//
 using mint = modint998244353;
 
-void solve(){
-		
+struct Node{
+	long long x,y;
+};
+vector<Node>a[200000];
+long long n,total,maxn,m,x,y,z;
+void dfs(long long x,long long f,long long s){
+	if(s>maxn){
+		maxn=s;
+		m=x;
+	}
+	for(int i=0;i<a[x].size();i++){
+		if(a[x][i].x!=f){
+			dfs(a[x][i].x,x,s+a[x][i].y);
+		}
+	}
 }
- 
+int solve(){
+	cin>>n;
+	for(int i=1;i<n;i++){
+		cin>>x>>y>>z;
+		total+=z;
+		a[x].push_back({y,z});
+		a[y].push_back({x,z});
+	}
+	dfs(1,0,0);dfs(m,0,0);
+	cout<<total*2-maxn;
+}
+
 int main() {
     solve();
     return 0;
---
+++ problems/abc362_c.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<queue>
+#include<stack>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+//*****taipt*****//
+using mint = modint998244353;
+
+void solve(){
+	int n; cin>> n;
+	vector<pair<long long, long long>> a;
+	for(int i =0;i<n;i++){
+		long long l,r;
+		cin>> l >> r;
+		a.push_back(make_pair(l,r));
+	}
+
+}
+ 
+int main() {
+    solve();
+    return 0;
+}
